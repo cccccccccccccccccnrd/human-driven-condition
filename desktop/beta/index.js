17c7bf2f2a1a7ef2a969d79b64dad0b6e6714cfb
cccccccccccccccccnrd <hello@conradweise.com>
2022-03-17 19:37:54 +0100

    progress

--- desktop/beta/index.js
+++ desktop/beta/index.js
@@ -3,26 +3,72 @@ const express = require('express')
 const WebSocket = require('ws')
 
 const app = express()
-const wss = new WebSocket.Server({ port: 2223 })
+const wss = new WebSocket.Server({
+  port: 2223
+})
 
 app.use('/beta/ui', express.static(path.join(__dirname, 'ui')))
 
 app.listen(2222)
 console.log('<-- human-driven condition (2021-) WWW listening on http://localhost:2222 WS listening on :2223')
 
+const state = {
+  devices: {}
+}
+
+function send (ws, type, payload) {
+  const msg = {
+    id: 'A00-SERVER',
+    type,
+    payload
+  }
+
+  ws.send(JSON.stringify(msg))
+}
+
+function broadcast (ws, type, payload) {
+  wss.clients.forEach((client) => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      send(client, type, payload)
+    }
+  })
+}
+
 wss.on('connection', (ws) => {
+  /* handle double connection from same device */
   ws.on('message', (message) => {
     const msg = JSON.parse(message)
     console.log(msg)
 
-    try {
-      console.log('do', msg.do)
-    } catch (error) {
-      console.log('f')
+    switch (msg.type) {
+      case 'get-state':
+        send(ws, 'state', {
+          state
+        })
+        break
+      case 'do':
+        broadcast(ws, 'do', msg.payload)
+        break
+      case 'open':
+        ws.id = msg.id
+        state.devices[msg.id] = {
+          id: msg.id
+        }
+        break
+      case 'closing':
+        delete state.devices[msg.id]
+        break
     }
   })
 
-  setInterval(() => {
+  ws.on('close', () => {
+    if (ws.id) {
+      console.log(ws.id, 'deleted')
+    }
+    delete state.devices[ws.id]
+  })
+
+  /* setInterval(() => {
     ws.send(JSON.stringify({
       do: 'toast'
     }))
@@ -32,5 +78,5 @@ wss.on('connection', (ws) => {
     ws.send(JSON.stringify({
       do: 'exit'
     }))
-  }, 11000)
+  }, 11000) */
 })
--- desktop/beta/ui/auto.js
+++ desktop/beta/ui/auto.js
@@ -1,15 +1,53 @@
 const url = window.location.hostname === 'localhost' ? 'ws://localhost:2223' : 'ws://192.168.1.127:2223'
 const socket = new WebSocket(url)
+const id = 'A00-CLIENT'
 
 const buttons = document.querySelectorAll('button')
+
+let state = {}
+
+socket.addEventListener('open', (message) => {
+  init()
+})
+
+socket.addEventListener('message', (message) => {
+  const msg = JSON.parse(message.data)
+  console.log(msg)
+
+  switch (msg.type) {
+    case 'state':
+      state = msg.payload.state
+      reloadUi()
+      break
+  }
+})
+
+function reloadUi () {
+  const deviceIds = Object.keys(state.devices)
+  document.querySelector('#state').innerHTML = deviceIds.map((id) => `<p>${id}</p>`).join('')
+}
+
+function send (type, payload) {
+  const msg = {
+    id,
+    type,
+    payload
+  }
+
+  socket.send(JSON.stringify(msg))
+}
+
+function init () {
+  send('get-state')
+}
 
 buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
     const id = event.target.id
     console.log(id)
-    socket.send(JSON.stringify({
+    send('do', {
       do: id
-    }))
+    })
     button.classList.toggle('done')
   })
 })
